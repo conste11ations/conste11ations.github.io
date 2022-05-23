60372915513d7751323701042ac8b08c62dd23cc
conste11ations <rachelkat@gmail.com>
2022-05-23 02:46:39 -0400

    out

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@
 // (But you could use ES2015 features supported by your Node.js version)
 
 module.exports = {
-  assetPrefix: '.',
+  assetPrefix: '/out/',
   images: {
     loader: "custom",
   },
