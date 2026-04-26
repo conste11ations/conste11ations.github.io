9dced33509356198adc35fcb0d84b3cf96179090
Rachel Kat <rachel.kat@dialogue.co>
2026-04-26 17:19:43 -0400

    Add debug logging to fetch-houses script

--- scripts/fetch-houses.js
+++ scripts/fetch-houses.js
@@ -58,6 +58,7 @@ function fetchListings(city) {
       let data = ''
       res.on('data', chunk => data += chunk)
       res.on('end', () => {
+        console.log(`  Raw response for ${city} (status ${res.statusCode}):`, data.slice(0, 500))
         try {
           resolve(JSON.parse(data))
         } catch (e) {
